7c089863332b1b0468bc03a4526f18aa72dc0311
Tixtor710 <robinrobert20038@gmail.com>
2025-06-30 23:50:15 +0530

    Minor changes to bot.py and Added a yaml file henceforth to host in render; Updated and frozen the requirements

--- bot.py
+++ bot.py
@@ -3,22 +3,28 @@ import requests
 import json
 
 def get_meme():
-  response = requests.get('https://meme-api.com/gimme')
-  json_data = json.loads(response.text)
-  return json_data['url']
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
 
 class MyClient(discord.Client):
-  async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
 
-  async def on_message(self, message):
-    if message.author == self.user:
-      return
-    if message.content.startswith('$meme'):
-      await message.channel.send(get_meme())
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
+<<<<<<< HEAD
 client.run('') # Replace with your own token
+=======
+
+# Run the client using the loaded token
+client.run(TOKEN)
+>>>>>>> 8c3ad9c (Minor changes to bot.py and Added a yaml file henceforth to host in render; Updated and frozen the requirements)
